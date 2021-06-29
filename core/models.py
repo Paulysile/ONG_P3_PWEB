146959ef0b76a970101f270ae366dd78c02fb77e
Paulysile <80928748+Paulysile@users.noreply.github.com>
2021-06-29 01:26:28 -0400

    prueba 3
    desarrollo

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from rest_framework.fields import ReadOnlyField
 
-# Create your models here.
+class Servicio(models.Model):
+    idServicio= models.IntegerField(primary_key= True, verbose_name='Id de Servicio' )
+    nombreServicio = models.CharField(max_length= 100, verbose_name= 'Servicio')
+
+
+    def __str__(self):
+        return self.nombreServicio
+
+class Proveedores(models.Model):
+    rut= models.CharField(max_length=10, primary_key=True, verbose_name='RUT')
+    razonsocial= models.CharField(max_length= 100, verbose_name= 'Razon Social')
+    descripcion= models.CharField(max_length=200, null= True, blank=True, verbose_name='Descripcion')
+    telefono= models.CharField(max_length=12, null= True, blank=True, verbose_name='Telefono')
+    email= models.EmailField(max_length=100, null= True, blank=True, verbose_name='E-mail')
+    servicio= models.ForeignKey(Servicio, on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.rut
--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import  inicio, contacto, formulario, enviado, exitoso, gatos, perros
+from .import views
+from .views import  inicio, contacto, formulario, enviado, exitoso, gatos, perros, proveedores, form_prov, form_mod_prov, form_del_prov
 
 
 urlpatterns = [
@@ -11,5 +12,9 @@ urlpatterns = [
     path('login-exitoso', exitoso, name="exitoso"),
     path('seccion-gatos', gatos, name="gatos"),
     path('seccion-perros', perros, name="perros"),
+    path('proveedores', proveedores, name="proveedores"),
+    path('form_prov', form_prov, name="form_prov"),
+    path('form_mod_prov/<id>', form_mod_prov, name="form_mod_prov"),
+    path('form_del_prov/<id>', form_del_prov, name="form_del_prov"),
 ]
 
